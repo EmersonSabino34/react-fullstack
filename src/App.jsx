28c686d39ec0db4853692c5ad5d10731640ae458
Emerson Sabino <emerson90_sabino@hotmail.com>
2024-07-15 22:18:43 +0100

    atualizaçao

--- src/App.jsx
+++ src/App.jsx
@@ -42,9 +42,35 @@ function App() {
       <table className='table table'>
         <thead>
           <tr>
-            
-          </tr>
+            <tr scope="col"></tr>
+            <th scope="col">Nome do Produto</th>
+            <th scope="col">Valor</th>
+            <th scope="col">Qtd</th>
+            <th scope="col">Url</th>
+            <th scope="col">Açoes</th>
+         </tr>
         </thead>
+        <tbody>
+          <tr>
+            <th scope='row'>1</th>
+            <td>Nome do Produto</td>
+            <td>Valor</td>
+            <td>Qtd</td>
+            <td>
+              <img width={40} src="https://d2e6ccujb3mkqf.cloudfront.net/a8312809-9dc0-4a28-be31-5f65dfa4d558-1_5e3d8b6c-9d8d-42df-a63a-7dd1111d0434.jpg" alt="imagem do Produto" />
+            </td>
+            <td>
+              <div className='btn-group d-flex gap-1'>
+                <button className='btn btn-outline-warning'>
+                <i className="fa-solid fa-pen-to-square"></i>
+                </button>
+                <button className='btn btn-outline-danger'>
+                <i className="fa-solid fa-trash"></i>
+                </button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
 
       </table>
 
